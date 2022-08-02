1b7f6c898039d248c2da0c4f66db3ed1919257fe
user <user@lggram>
2022-08-03 00:20:30 +0900

    my new type Hashmap-Arraylist-test

--- src/array/array.java
+++ src/array/array.java
@@ -2,113 +2,148 @@ package array;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class array {
-
-	public static void main(String[] args) {
-		// 1번문제
+	public static void arr1() {
 		List<Integer> arr1 = new ArrayList<Integer>();
+		System.out.println("Array 1번문제 입니다.");
 		arr1.add(1);
 		arr1.add(2);
 		arr1.add(3);
 		arr1.add(4);
 		arr1.add(5);
-		System.out.println("어레이 1번 문제 입니다.");
 		for (int i = 0; i < arr1.size(); i++) {
 			System.out.println(arr1.get(i));
 		}
 		System.out.println(arr1);
+	}
+
+	public static void arr2() {
+		System.out.println("Array 2번문제 입니다.");
 		List<Integer> arr2 = new ArrayList<Integer>();
-		// 2번문제
 		arr2.add(1);
 		arr2.add(2);
 		arr2.add(3);
 		arr2.add(4);
 		arr2.add(5);
-		System.out.println("어레이 2번 문제입니다.");
-		for (
 
-				int i = 0; i <= arr2.size(); i++) {
-			if (0 == arr2.get(i) % 2) {
+		for (int i = 0; i < arr2.size(); i++) {
+			if (arr2.get(i) % 2 == 0) {
 				System.out.println(arr2.get(i));
 			}
-
 		}
-		// 3문제
-		List<Integer> arr3 = new ArrayList<Integer>();
+	}
 
+	public static void arr3() {
+		System.out.println("Array 3번문제 입니다.");
+		List<Integer> arr3 = new ArrayList<Integer>();
 		arr3.add(1);
 		arr3.add(2);
 		arr3.add(3);
 		arr3.add(4);
 		arr3.add(5);
-		System.out.println("어레이3번문제입니다.");
-		for (
-
-				int i = 0; i <= arr3.size(); i++) {
-			if (0 == arr3.get(i) % 2) {
+		for (int i = 0; i < arr3.size(); i++) {
+			if (arr3.get(i) % 2 == 0) {
 				System.out.println(arr3.get(i) * 2);
 			}
-
 		}
-		// 4번문제
-		List<String> arr4 = new ArrayList<String>();
+	}
 
+	public static void arr4() {
+		System.out.println("Array 4번문제 입니다.");
+		List<String> arr4 = new ArrayList<String>();
 		arr4.add("1");
 		arr4.add("2");
 		arr4.add("3");
 		arr4.add("4");
 		arr4.add("5");
-		System.out.println("어레이4번문제입니다.");
-		for (int i = 0; i <= arr4.size(); i++) {
-			if (Integer.valueOf(arr4.get(i)) % 2 == 1) {
-				System.out.print(arr4.get(i) + "-");
-				System.out.println(Integer.valueOf(arr4.get(i)) + 2);
-			} else {
-				System.out.println("짝수입니다.");
-			}
 
+		for (int i = 0; i < arr4.size(); i++) {
+			if (Integer.parseInt(arr4.get(i)) % 2 == 0) {
+				System.out.println("짝수입니다.");
+			} else {
+				System.out.print(arr4.get(i) + "->");
+				System.out.println((Integer.parseInt(arr4.get(i)) + 2));
+			}
 		}
-//		5번문제
+	}
+
+	public static void arr5() {
+		System.out.println("Array 5번문제 입니다.");
 		List<String> arr5 = new ArrayList<String>();
 		arr5.add("사과");
 		arr5.add("수박");
 		arr5.add("바나나");
 		arr5.add("참외");
 		arr5.add("딸기");
-		System.out.println("어레이 5번 문제 입니다.");
-		for (int i = 0; i <= arr5.size(); i++) {
-			if (arr5.get(i) == "사과") {
-				System.out.println(arr5.get(i) + "는 맛있어요");
-			} else if (arr5.get(i) == "수박") {
-				System.out.println("아보카도");
-			} else if (arr5.get(i) == "딸기") {
-				System.out.println(arr5.get(i) + "는 맛있어요");
+
+		for (int i = 0; i < arr5.size(); i++) {
+			if (arr5.get(i).equals("사과") || arr5.get(i).equals("딸기")) {
+				System.out.println(arr5.get(i) + "는 맛있어요.");
+
+			} else if (arr5.get(i).equals("수박")) {
+				System.out.println(" 아보카도");
+
 			} else
 				System.out.println(arr5.get(i));
-
 		}
-		//6번문제
+	}
+
+	public static void arr6() {
+		System.out.println("Array 5번문제 입니다.");
 		List<Integer> arr6 = new ArrayList<Integer>();
-		arr6.add(12);
-		arr6.add(23);
+		arr6.add(1);
+		arr6.add(2);
 		arr6.add(3);
-		arr6.add(40);
-		arr6.add(15);
-		arr6.add(26);
-		arr6.add(27);
+		arr6.add(4);
+		arr6.add(5);
+		arr6.add(6);
+		arr6.add(7);
 		arr6.add(8);
-		arr6.add(93);
+		arr6.add(9);
 		arr6.add(10);
-		System.out.println("어레이 6번문제 입니다.");
+		
 		int i = 0;
-		while (i <= arr6.size()) {
+		while (i < arr6.size() - 1) {
 			int a = arr6.get(i);
 			i++;
 			int s = arr6.get(i);
-			System.out.println(String.format("%d 는 %d 보다%d만큼 작다", a, s, a - s));
+			System.out.println(String.format("%d 는 %d 보다%d만큼 작다", a, s, s - a));
+		}
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println("보고싶은 출력의 번호를 입력해주세요(1~6까지 입력)");
+		Scanner sc = new Scanner(System.in);
+		int check = sc.nextInt();
+		switch (check) {
+		case 1:
+			arr1();
+			break;
+		case 2:
+			arr2();
+			break;
+		case 3:
+			arr3();
+			break;
+		case 4:
+			arr4();
+			break;
+		case 5:
+			arr5();
+			break;
+		case 6:
+			arr6();
+			break;
+
+		default:
+			System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
 
 		}
+		sc.close();
+
 	}
 
 }
--- src/hash/hash.java
+++ src/hash/hash.java
@@ -2,49 +2,67 @@ package hash;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class hash {
-
-	public static void main(String[] args) {
-		//1번문제
-		Map<Integer, String> map1 = new HashMap<Integer, String>();
-		map1.put(1, "첫 번째 타자");
-		map1.put(2, "두 번째 타자");
-		map1.put(3, "세 번째 타자");
-		map1.put(4, "네 번째 타자");
-		map1.put(5, "다섯 번째 타자");
-		System.out.println("해시 첫번째 문제");
-		for (int i = 1; i < map1.size() + 1; i++) {
-			System.out.println(map1.get(i));
-		}
+	public static void map1() {
+	Map<Integer, String> map1 = new HashMap<Integer, String>();
+	System.out.println("Hash 첫번째 문제입니다.");
+	map1.put(1, "첫 번째 타자");
+	map1.put(2, "두 번째 타자");
+	map1.put(3, "세 번째 타자");
+	map1.put(4, "네 번째 타자");
+	map1.put(5, "다섯 번째 타자");
+	 
+	for(int i =1; i < map1.size()+1 ; i++) {
+		System.out.println(map1.get(i));
+	}
 		System.out.println(map1);
-		//2문제
+	}
+	public static void map2() {
 		Map<String, String> map2 = new HashMap<String, String>();
-
+		System.out.println("Hash 두번쨰 문제 입니다.");
 		map2.put("1", "첫 번째 타자");
 		map2.put("2", "두 번째 타자");
 		map2.put("3", "세 번째 타자");
 		map2.put("4", "네 번째 타자");
 		map2.put("5", "다섯 번째 타자");
-		System.out.println("해시 두번째 문제");
-
-		for (String s : map2.keySet()) {
-
-			System.out.println(s + "=" + map2.get(s));
+		for(int i = 1; i < map2.size()+1; i++) {
+			System.out.println(map2.get(i+""));
 		}
-		//3번문제
-		Map<Object, String> map3 = new HashMap<Object, String>();
-		map3.put(1, "첫 번째 타자");
-		map3.put(2, "두 번째 타자");
-		map3.put(3, "세 번째 타자");
-		map3.put(4, "네 번째 타자");
-		map3.put(5, "다섯 번째 타자");
-
-		System.out.println("해시 세번째 문제");
-		for (Object s : map3.keySet()) {
-
-			System.out.println(s + "=" + map3.get(s));
+	}
+	public static void map3() {
+		System.out.println("Hash 두번쨰 문제 입니다.");
+		Map<Object, Object> map3 = new HashMap<Object, Object>();
+		map3.put(1, "첫번쨰 타자 입니다.");
+		map3.put("2", "두번쨰 타자 입니다.");
+		map3.put(3, "세번쨰 타자 입니다.");
+		map3.put("4", "네번쨰 타자 입니다.");
+		map3.put(5, "다섯번쨰 타자 입니다.");
+		for(int i= 1 ; i < map3.size()+1; i++) {
+			if(map3.get(i) != null) {
+				System.out.println(map3.get(i));
+			}
+			else 
+				System.out.println(map3.get(i+""));
+			
+			}
 		}
+	
+	public static void main(String[] args) {
+		System.out.println("보고싶은 출력의 번호를 입력해주세요(1~3까지 입력)");
+		Scanner sc = new Scanner(System.in);
+		int check = sc.nextInt();
+		switch (check) {
+		case 1: map1(); break;
+		case 2: map2(); break;
+		case 3: map3(); break;
+		default: System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
+		
+		}
+		sc.close();
+		
+
 	}
 
 }
